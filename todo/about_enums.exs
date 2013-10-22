5db0273e5713e0b3b5ab2730889b26656d153b58
Grégory Salvan <apieum@gmail.com>
2013-10-22 18:15:59 +0200

    Added todos

---
+++ todo/about_enums.exs
@@ -0,0 +1,38 @@
+'''
+@TODO
+- Streams
+- empty?
+- all?
+- any?
+- member?
+- at
+- with_index
+- chuncks and chunks_by
+- concat
+- count 1/2
+- drop
+- drop_while
+- each
+- fetch and fetch!
+- filter
+- filter_map
+- find, find_index, find_value
+- first
+- flat_map
+- join
+- map
+- map_join, map_reduce
+- zip
+- max, max_by, min, min_by
+- partition
+- reduce
+- reject
+- reverse
+- shuffle
+- slice
+- sort
+- uniq
+- split and split_while
+- take, take_every, take_while
+
+'''
---
+++ todo/about_pattern_matching.exs
@@ -0,0 +1,10 @@
+'''
+@TODO
+- about =
+- Case
+- tupples:
+    { :ok, result } = { :ok, 13 }
+
+- lists:
+    [head | tail] = [1,2,3]
+'''

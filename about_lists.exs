f2e82966c6077dd23c193f6e7b34429aaa77f037
Grégory Salvan <apieum@gmail.com>
2013-10-22 18:15:09 +0200

    Truncate and Concatenate lists

--- about_lists.exs
+++ about_lists.exs
@@ -4,4 +4,35 @@ defmodule About_Lists do
     use ExUnit.Case
     use Koans
 
+    test "Create your first list" do
+        a_list = __?
+        assert is_list(a_list)
+    end
+
+    test "Getting list lenght is a kernel feature" do
+        a_list = [1, 2, 3]
+        assert length(a_list) == __?
+    end
+
+    test "Elixir provide special operator to concatenate lists" do
+        a_list = [1, 2]
+        assert a_list ++ [3] == __?
+    end
+
+    test "Elixir provide special operator to remove element from list" do
+        a_list = [1, 2, 3]
+        assert a_list -- [2] == __?
+    end
+
+    test "Only first element is removed with truncate operator" do
+        a_list = [:foo, :bar, :foo]
+        assert a_list -- [:foo] == __?
+    end
+
+    test "Truncate operator do nothing when element not in list" do
+        a_list = [:foo, :bar]
+        __? a_list -- [:baz] == [:foo, :bar]
+    end
+
+
 end

9645cf13354309fa6e9688bc86916de4945e8aab
Ryan Bigg <git@ryanbigg.com>
2015-10-01 00:22:49 +1000

    Add tuple + head|tail example to about_match_variables

--- about_match_variables.exs
+++ about_match_variables.exs
@@ -3,40 +3,50 @@
 ExUnit.start
 
 defmodule About_Matches_Variables do
-    use ExUnit.Case
-    use Koans
+  use ExUnit.Case
+  use Koans
 
-    think "It's like affecting a variable" do
-        a_variable = 1
-        assert a_variable == __?
-    end
+  think "It's like affecting a variable" do
+    a_variable = 1
+    assert a_variable == __?
+  end
 
-    think "The real operation is named matching" do
-        [a, b] = [1, 2]
+  think "The real operation is named matching" do
+    [a, b] = [1, 2]
 
-        assert a  == __? and b == __?
-    end
+    assert a  == __? and b == __?
+  end
 
-    think "Matching three variables" do
-        [a, b, c] = [1, 2, 3]
+  think "Matching three variables" do
+    [a, b, c] = [1, 2, 3]
 
-        assert a  == __? and b == __? and c == __?
-    end
+    assert a  == __? and b == __? and c == __?
+  end
 
-    think "Matching one variable and drop a value" do
-        [a, _] = ["Keep me", "Drop me"]
+  think "Matching one variable and drop a value" do
+    [a, _] = ["Keep me", "Drop me"]
 
-        assert a  == __?
-    end
+    assert a  == __?
+  end
 
-    think "Error matching" do
+  think "Error matching" do
+    assert_raise __?, fn -> [ a , a ] = [ 1 , 2 ] end
+  end
 
-        assert_raise __?, fn -> [ a , a ] = [ 1 , 2 ] end
-    end
+  think "Matching an array inner an array" do
+    [ a ] = [ ["Hello", "World !"] ]
 
-    think "Matching an array inner an array" do
-        [ a ] = [ ["Hello", "World !"] ]
+    assert a  == __?
+  end
 
-        assert a  == __?
-    end
+  think "matching a tuple" do
+    { :ok, result } = { :ok, 12 }
+    assert result == __?
+  end
+
+  think "heads and tails" do
+    [ head | tail ] = [1, 2, 3]
+    assert head == __?
+    assert tail == __?
+  end
 end
